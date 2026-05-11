5d51ccb603c81221c26766c5b710a37a81afad96
Lev Gorlov <lev.gorlov@gmail.com>
2026-05-11 13:21:25 +0500

    feat(stage3): T41 start CallObserverService at app launch when permissions granted

--- app/src/main/kotlin/com/qalqan/antifraud/MainActivity.kt
+++ app/src/main/kotlin/com/qalqan/antifraud/MainActivity.kt
@@ -3,12 +3,21 @@ package com.qalqan.antifraud
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.qalqan.antifraud.calls.CallObserverPermissions
+import com.qalqan.antifraud.calls.CallObserverService
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             StatusScreen()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (CallObserverPermissions(this).state() == CallObserverPermissions.State.GRANTED) {
+            CallObserverService.start(this)
         }
     }
 }
