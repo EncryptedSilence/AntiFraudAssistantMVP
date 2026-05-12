a6bd7f05b30bafe15070470fb9ee6174dcdb9f8d
Lev Gorlov <lev.gorlov@gmail.com>
2026-05-12 13:05:25 +0500

    feat(stage6): T20 add :core:sync module skeleton with INTERNET permission

---
+++ core/sync/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("antifraud.android.library")
+}
+
+android {
+    namespace = "com.qalqan.antifraud.sync"
+}
+
+dependencies {
+    implementation(project(":core:crypto"))
+    implementation(project(":core:patterns"))
+    implementation(project(":core:database"))
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+}
