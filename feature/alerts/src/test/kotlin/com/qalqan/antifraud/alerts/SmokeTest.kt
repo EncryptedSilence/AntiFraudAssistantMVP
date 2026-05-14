ba473c749ccdf632bdddbfba2d0df46662aa3f89
Lev Gorlov <lev.gorlov@gmail.com>
2026-05-14 13:16:36 +0500

    feat(stage9): T03 register :feature:alerts and add smoke test

---
+++ feature/alerts/src/test/kotlin/com/qalqan/antifraud/alerts/SmokeTest.kt
@@ -0,0 +1,11 @@
+package com.qalqan.antifraud.alerts
+
+import io.kotest.matchers.shouldBe
+import org.junit.Test
+
+class SmokeTest {
+    @Test
+    fun `module is wired`() {
+        2 + 2 shouldBe 4
+    }
+}
